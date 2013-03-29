c24652f502855092550ff1e2aae7ef07e1201836
Jason Larsen <jasonklarsen@gmail.com>
2013-03-29 14:29:23 -0400

    Adding icons to main page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,7 +9,9 @@ function cleanData(data) {
 
 function ReportListCtrl($scope, $http) {
   $http.get('data/reports_latest.json').success(function(data) {
-    $scope.reports = cleanData(data);
+    var parsedData = cleanData(data);
+    addAllContextsTo(parsedData);
+    $scope.reports = parsedData;
   });
 
   $scope.orderProp = 'age';
@@ -17,12 +19,32 @@ function ReportListCtrl($scope, $http) {
 
 //ReportListCtrl.$inject = ['$scope', '$http'];
 
-
 function ReportDetailCtrl($scope, $routeParams, $http) {
   $scope.uuid = $routeParams.uuid;
   $http.get('data/' + $routeParams.uuid + '.json').success(function(data) {
-    $scope.report = cleanData(data);
+    var parsedData = cleanData(data);
+    addContextTo(parsedData);
+    $scope.report = parsedData;
   });
 }
 
 //ReportDetailCtrl.$inject = ['$scope', '$routeParams', '$http'];
+
+function addAllContextsTo(allData) {
+  angular.forEach(allData, function(data) {
+    addContextTo(data);
+  })
+}
+
+function addContextTo(data) {
+  data.iconFailures = "icon-ok";
+  data.iconChanges = "icon-minus";
+  if (data.fields.metrics.resources.Failed > 0) { 
+    data.hadFailures = "hadFailures";
+    data.iconFailures = "icon-remove";
+  }
+  if (!data.fields.metrics.events.Noop || data.fields.metrics.events.Noop < 1) { 
+    data.hadChanges = "hadChanges";
+    data.iconChanges = "icon-exclamation-sign";
+  }
+}
