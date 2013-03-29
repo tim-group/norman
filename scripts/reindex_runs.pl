d87795a6c0c39e4c00714d436ad740fd267d4a13
Tomas Doran <bobtfish@bobtfish.net>
2013-03-29 13:13:35 -0400

    Re-index script

--- scripts/reindex_runs.pl
+++ scripts/reindex_runs.pl
@@ -2,36 +2,21 @@
 use  strict;
 use warnings;
 use lib '/opt/message-passing/lib/perl5/';
-use JSON qw/ encode_json to_json /;
-use Data::GUID;
+use JSON qw/ encode_json to_json from_json /;
+use FindBin qw/ $Bin /;
 
 use ElasticSearch;
-
+open(my $fh, '<', "$Bin/../app/data/reports_latest.json");
+my $data = from_json(do { local $/; local $\; <$fh> });
 my $es = ElasticSearch->new( servers => 'localhost:9200' );
 #$es->trace_calls(1); 
-my $results = $es->search(
-  index => 'logstash-2013.03.29',
-  type  => 'puppet-apply',
-  "from" => 0, "size" => 1000,
-  query => {
-        "term" => { '@tags' => "puppet-apply" }
-  }
-);
-use Data::Dumper;
-
-#warn Dumper($results);
-
-my @data = map { $_->{_source} } @{ $results->{hits}{hits} };
-
-system("rm -r /tmp/test_data; mkdir /tmp/test_data");
-foreach my $datum (@data) {
-    $datum->{'@uuid'} = Data::GUID->new()->as_string;
-    open(my $fh, '>', "/tmp/test_data/" . $datum->{'@uuid'} . ".json") or die;
-    print $fh to_json($datum, {utf8 => 1, pretty => 1}) . "\n";
-    close($fh);
+foreach my $datum (@$data) {
+  $es->index(
+    index => 'logstash-2013.03.29',
+    type  => 'puppet-apply',
+    id => $datum->{'@uuid'},
+    data => $datum,
+  );
 }
-open(my $fh, '>', "/tmp/test_data/reports_latest.json") or die;
-print $fh to_json(\@data, {utf8 => 1, pretty => 1}) . "\n";
-close($fh);
 
 
