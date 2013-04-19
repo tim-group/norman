e8a53f10e8851cf8d2dbd93131c49b11302fc4e6
Tomas Doran <bobtfish@bobtfish.net>
2013-04-19 11:29:32 -0700

    Get the controllers to not be global functions, breaks the tests currently

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,10 @@
 
 /* App Module */
 
-angular.module('norman', ['normanServices']).
+window.norman = angular.module('norman', ['normanServices']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/latest', {templateUrl: 'partials/report-latest.html',   controller: ReportListCtrl}).
-      when('/report/:index/:uuid', {templateUrl: 'partials/report-detail.html', controller: ReportDetailCtrl}).
+      when('/latest', {templateUrl: 'partials/report-latest.html',   controller: 'ReportListCtrl'}).
+      when('/report/:index/:uuid', {templateUrl: 'partials/report-detail.html', controller: 'ReportDetailCtrl'}).
       otherwise({redirectTo: '/latest'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,7 +2,8 @@
 
 /* Controllers */
 
-function ReportListCtrl($scope, $http, ESHelpers) {
+window.norman.
+  controller('ReportListCtrl', function ($scope, $http, ESHelpers) {
   $http.post(ESHelpers.elasticsearch_url() + '/_all/puppet-apply/_search', angular.toJson({
    "from" : 0, "size" : 100,
    "query": {
@@ -23,16 +24,14 @@ function ReportListCtrl($scope, $http, ESHelpers) {
   });
 
   $scope.orderProp = '-timestamp';
-}
-
-//ReportListCtrl.$inject = ['$scope', '$http', 'ESHelpers'];
-
-function ReportDetailCtrl($scope, $routeParams, $http, ESHelpers) {
+}).
+controller('ReportDetailCtrl', function ($scope, $routeParams, $http, ESHelpers) {
   $scope.uuid = $routeParams.uuid;
   $http.get(ESHelpers.elasticsearch_url() + '/' + $routeParams.index + '/puppet-apply/' + $routeParams.uuid).success(function(data) {
     $scope.report = ESHelpers.addContextTo(ESHelpers.cleanData(data["_source"]));
   });
-}
+});
 
+//ReportListCtrl.$inject = ['$scope', '$http', 'ESHelpers'];
 //ReportDetailCtrl.$inject = ['$scope', '$routeParams', '$http', 'ESHelpers'];
 
