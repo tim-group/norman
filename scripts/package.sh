10433488d9dcf1344fc6c7befe1a45853ab1beef
Tomas Doran <bobtfish@bobtfish.net>
2013-04-21 13:48:38 -0700

    Run package.sh from travis, commit to gh-pages

--- scripts/package.sh
+++ scripts/package.sh
@@ -11,4 +11,5 @@ cp -r "$packagefn" gh-pages/releases
 git add -f gh-pages
 tree=$(git write-tree --prefix=gh-pages/) && commit=$(echo \"Generated docs\" | git commit-tree $tree -p gh-pages) && git update-ref refs/heads/gh-pages $commit && git reset HEAD
 if [ -d gh-pages ]; then rm -r gh-pages; fi
+echo "Committed tree ${tree} containing ${packagefn} into gh-pages/release"
 
